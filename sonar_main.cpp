4891c28ff39a70347d59dd9ad7de7b7cefe7dd0f
Andrew Herren <andrew.herren@gmail.com>
2025-10-14 07:02:30 -0400

    ci debug

---
+++ sonar_main.cpp
@@ -0,0 +1,9 @@
+// This file forces compilation of everything
+#include "ArduinoFake.h"
+#include "ArduinoJson.h"
+#include "web_platform_interface/interface/web_request_constants.h"
+#include "web_platform_interface/testing/route_variant_native.h"
+#include "web_platform_interface/testing/web_response_native.h"
+
+
+int main() { return 0; }
